5eda7e57b4882e058d943e6903490b2808dc9f54
Dax Sorbito <daxsorbito@gmail.com>
2015-11-13 01:55:34 +0800

    fixed partial views and fixed the directive

--- modules/core/client/config/core-directive.client.controls.js
+++ modules/core/client/config/core-directive.client.controls.js
@@ -2,19 +2,21 @@ angular.module('core').directive('primaryservice', function() {
     'use strict';
     return {
         restrict: 'E',
-        replace: 'true',
-        template: '<select data-ng-model="user.primaryService" id="primaryService" class="form-control">' +
-                        '<option value="0" {{(user.primaryService == 0) ? "selected" : ""}}>{{ "PLEASE_SELECT" | translate }}</option>' +
-                        '<option value="1" {{(user.primaryService == 1) ? "selected" : ""}}>{{ "SERVICES.HOUSE_CLEANER" | translate }}</option>' +
-                        '<option value="2" {{(user.primaryService == 2) ? "selected" : ""}}>{{ "SERVICES.LAUNDRY_PRESS" | translate }}</option>' +
-                        '<option value="3" {{(user.primaryService == 3) ? "selected" : ""}}>{{ "SERVICES.CARETAKER_OLDSITTER" | translate }}</option>' +
-                        '<option value="4" {{(user.primaryService == 4) ? "selected" : ""}}>{{ "SERVICES.CARETAKER_BABYSITTER" | translate }}</option>' +
-                        '<option value="5" {{(user.primaryService == 5) ? "selected" : ""}}>{{ "SERVICES.PLUMBER" | translate }}</option>' +
-                        '<option value="6" {{(user.primaryService == 6) ? "selected" : ""}}>{{ "SERVICES.AIRCON_CLEANER" | translate }}</option>' +
-                        '<option value="7" {{(user.primaryService == 7) ? "selected" : ""}}>{{ "SERVICES.HOUSE_FIXTURES_INSTALLER" | translate }}</option>' +
-                        '<option value="8" {{(user.primaryService == 8) ? "selected" : ""}}>{{ "SERVICES.GARDENER" | translate }}</option>' +
-                        '<option value="9" {{(user.primaryService == 9) ? "selected" : ""}}>{{ "SERVICES.HANDYMAN" | translate }}</option>' +
-                        '<option value="9" {{(user.primaryService == 9) ? "selected" : ""}}>DAXFAXDAX</option>' +
+        scope: {
+            ngModel: '=',
+            id: '='
+        },
+        template: '<select ng-model="ngModel" id="id" class="form-control">' +
+                        '<option value="0" ng-selected="(ngModel || 0) == 0">{{ "PLEASE_SELECT" | translate }}</option>' +
+                        '<option value="1" ng-selected="ngModel == 1">{{ "SERVICES.HOUSE_CLEANER" | translate }}</option>' +
+                        '<option value="2" disabled ng-selected="ngModel == 2">{{ "SERVICES.LAUNDRY_PRESS" | translate }}</option>' +
+                        '<option value="3" disabled ng-selected="ngModel == 3">{{ "SERVICES.CARETAKER_OLDSITTER" | translate}}</option>' +
+                        '<option value="4" disabled ng-selected="ngModel == 4">{{ "SERVICES.CARETAKER_BABYSITTER" | translate}}</option>' +
+                        '<option value="5" disabled ng-selected="ngModel == 5">{{ "SERVICES.PLUMBER" | translate }}</option>' +
+                        '<option value="6" disabled ng-selected="ngModel == 6">{{ "SERVICES.AIRCON_CLEANER" | translate }}</option>' +
+                        '<option value="7" disabled ng-selected="ngModel == 7">{{ "SERVICES.HOUSE_FIXTURES_INSTALLER" | translate }}</option>' +
+                        '<option value="8" disabled ng-selected="ngModel == 8">{{ "SERVICES.GARDENER" | translate }}</option>' +
+                        '<option value="9" disabled ng-selected="ngModel == 9">{{ "SERVICES.HANDYMAN" | translate }}</option>' +
                     '</select>'
 
     };
